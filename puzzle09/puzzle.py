f8325408d11fc305b8e7b622f92a41a3e99dae50
Mike Mudge <mike.mudge@gmail.com>
2019-12-29 14:19:08 +1300

    Add day 9 solution.

---
+++ puzzle09/puzzle.py
@@ -0,0 +1,175 @@
+import math
+import os
+import itertools
+
+class Computer:
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
+        self.complete = False
+        self.relative_base = 0
+
+    def readMode(self, mode, idx):
+        if mode == 2:
+            # Relative mode.
+            return self.data[self.relative_base + self.data[idx]]
+        elif mode == 1:
+            # Immediate mode.
+            return self.data[idx]
+        elif mode == 0:
+            # Position mode.
+            pos = self.data[idx]
+            size = pos + 1 - len(self.data)
+            if size > 0:
+                print("Growing memory to fit", pos)
+                # Grow the memory to fit everything.
+                self.data.extend([0] * size)
+            return self.data[pos]
+        else:
+            raise Exception("Unknown mode")
+
+    def saveMode(self, mode, idx, value, verbose=False):
+        if mode == 1:
+            raise Exception("Immediate mode not supported for write")
+        elif mode == 2:
+            if verbose:
+                print("self.data[", self.relative_base + idx, "]=", value)
+            self.data[self.relative_base + idx] = value
+        elif mode == 0:
+            print("self.data[", idx, "]=", value)
+            self.data[idx] = value
+        else:
+            raise Exception("Unknown mode")
+
+    def runComputer(self, inputs, verbose=False):
+        outputs = []
+        while not self.complete:
+            o = self.runComputerUntilOutput(inputs, verbose=verbose)
+            if o is not None:
+                outputs.append(o)
+        return outputs
+
+    def runComputerUntilOutput(self, inputs, verbose=False):
+        while self.data[self.index] != 99:
+            opcode = self.data[self.index]
+
+            op = opcode % 100
+            mode1 = opcode / 100 % 10
+            mode2 = opcode / 1000 % 10
+            mode3 = opcode / 10000 % 10
+
+            if verbose:
+                print(self.index, "op", opcode)
+
+            if op == 1:
+                a = self.readMode(mode1, self.index + 1)
+                b = self.readMode(mode2, self.index + 2)
+                c = self.data[self.index + 3]
+                self.saveMode(mode3, c, a + b, verbose)
+                self.index += 4
+            elif op == 2:
+                a = self.readMode(mode1, self.index + 1)
+                b = self.readMode(mode2, self.index + 2)
+                c = self.data[self.index + 3]
+                if verbose:
+                    print("self.data[", c, "]=", a * b, a, "*", b)
+                self.saveMode(mode3, c, a * b)
+                self.index += 4
+            elif op == 3:
+                a = self.data[self.index + 1]
+                value = inputs.pop()
+                if verbose:
+                    print("self.data[", a, "] read input ", value)
+                self.saveMode(mode1, a, value)
+                self.index += 2
+            elif op == 4:
+                a = self.readMode(mode1, self.index + 1)
+                if verbose:
+                    print("out", a)
+                self.index += 2
+                self.complete = self.data[self.index] == 99
+                return a
+            elif op == 5:
+                # Jump if not equal to 0.
+                a = self.readMode(mode1, self.index + 1)
+                if a != 0:
+                    b = self.readMode(mode2, self.index + 2)
+                    if verbose:
+                        print(a, "!=", 0, "jumpto", self.data[self.index + 2], mode2, b)
+                    self.index = b
+                else:
+                    self.index += 3
+                    if verbose:
+                        print(a, "is", 0, "no-op")
+            elif op == 6:
+                # Jump if equals 0
+                a = self.readMode(mode1, self.index + 1)
+                if a == 0:
+                    b = self.readMode(mode2, self.index + 2)
+                    self.index = b
+                    if verbose:
+                        print(a, "==", 0, "jumpto", self.index)
+                else:
+                    self.index += 3
+                    if verbose:
+                        print(a, "is not", 0, "no-op")
+            elif op == 7:
+                a = self.readMode(mode1, self.index + 1)
+                b = self.readMode(mode2, self.index + 2)
+                c = self.data[self.index + 3]
+                if verbose:
+                    print("self.data[", c, ']=', int(a < b), "because", a, "<", b)
+                if a < b:
+                    self.saveMode(mode3, c, 1)
+                else:
+                    self.saveMode(mode3, c, 0)
+                self.index += 4
+            elif op == 8:
+                a = self.readMode(mode1, self.index + 1)
+                b = self.readMode(mode2, self.index + 2)
+                c = self.data[self.index + 3]
+                if verbose:
+                    print(c, '=', a, "==", b, a == b)
+                if a == b:
+                    self.saveMode(mode3, c, 1)
+                else:
+                    self.saveMode(mode3, c, 0)
+                self.index += 4
+            elif op == 9:
+                a = self.readMode(mode1, self.index + 1)
+                if verbose:
+                    print ("rel", self.relative_base, "+=", a)
+                self.relative_base += a
+                self.index += 2
+            else:
+                raise Exception("Unknown op %d in %d" % (op, opcode))
+
+        self.complete = True
+
+path = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(path, "input")) as file:
+    lines = file.readlines()
+
+# Samples
+# lines = ["109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"]
+# lines = ["1102,34915192,34915192,7,4,7,99,0"]
+# lines = ["104,1125899906842624,99"]
+
+data = lines[0].split(",")
+data = list(map(int, data))
+print("program with", len(data), "slots")
+
+inputs = [1]
+c = Computer(data[:])
+c.data.extend([0] * 100)
+outputs = c.runComputer(inputs, verbose=True)
+
+# 203 was too low.
+print("Part 1:", outputs[0])
+
+inputs = [2]
+c = Computer(data[:])
+c.data.extend([0] * 1000)
+outputs = c.runComputer(inputs, verbose=True)
+
+print("Part 2:", outputs[0])
